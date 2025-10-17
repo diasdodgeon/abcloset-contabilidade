4703170f068ea18c4391303a8cde0d4bba856ff6
diasdodgeon <145266779+diasdodgeon@users.noreply.github.com>
2025-10-17 16:07:28 -0300

    Add files via upload

---
+++ abcloset-contabilidade/config.js
@@ -0,0 +1,16 @@
+
+
+export const FIREBASE_CONFIG = {
+  apiKey: "AIzaSyBE7Swcwkx_B7IAROPuC6sIdWpxyEC3r_Q",
+  authDomain: "contabilidade-abcloset.firebaseapp.com",
+  projectId: "contabilidade-abcloset",
+  storageBucket: "contabilidade-abcloset.firebasestorage.app",
+  messagingSenderId: "69860840165",
+  appId: "1:69860840165:web:cb4694c4d73da77b55fa0c",
+  measurementId: "G-YFSDS23CMY"
+};
+export const GITHUB_CONFIG = {
+  repoOwner: "diasdodgeon",
+  repoName: "abcloset-imagens",
+  token: "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",
+};
